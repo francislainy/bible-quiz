c1d0d0bcdbcf39f60af8bda9ce1dd77df8df1e50
Francislainy <francislainy.campos@gmail.com>
2025-01-26 06:17:23 +0000

    Move routes to package route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {SubjectListComponent} from './components/subject-list/subject-list.component';
+import {SubjectComponent} from './components/subject/subject.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'subjects'},
+  {path: 'subjects', component: SubjectListComponent},
+  {path: 'subject/:id', component: SubjectComponent},
+];
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
-import {SubjectListComponent} from './app/components/subject-list/subject-list.component';
-import {SubjectComponent} from './app/components/subject/subject.component';
-import {provideRouter, Routes} from '@angular/router';
-
-const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'subjects'},
-  {path: 'subjects', component: SubjectListComponent},
-  {path: 'subject/:id', component: SubjectComponent},
-];
+import {provideRouter} from '@angular/router';
+import {routes} from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)],
